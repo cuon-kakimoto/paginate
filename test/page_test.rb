825e3c7b80389173ecee021a9c90423a7ddbf04b
cuon-kakimoto <skakimoto@cuon.co.jp>
2024-03-25 21:09:37 +0900

    13:47 Testing our engine with the test/dummy application

--- test/page_test.rb
+++ test/page_test.rb
@@ -5,4 +5,22 @@ class PageTest < ActiveSupport::TestCase
     assert_equal Recording.first(30), Paginate::Page.new(1, Recording.all).records
     assert_equal Recording.last(20), Paginate::Page.new(4, Recording.all).records
   end
+
+  test "previous_param" do
+    assert_equal 1, Paginate::Page.new(2, Recording.all).previous_param
+  end
+
+  test "next_param" do
+    assert_equal 3, Paginate::Page.new(2, Recording.all).next_param
+  end
+
+  test "first?" do
+    assert Paginate::Page.new(1, Recording.all).first?
+    assert_not Paginate::Page.new(2, Recording.all).first?
+  end
+
+  test "last?" do
+    assert Paginate::Page.new(4, Recording.all).last?
+    assert_not Paginate::Page.new(3, Recording.all).last?
+  end
 end
