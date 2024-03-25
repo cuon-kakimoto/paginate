143657aa919d3835e51b93b578bcd78e7797549d
cuon-kakimoto <skakimoto@cuon.co.jp>
2024-03-25 21:24:29 +0900

    27:57 Manual tests with the test/dummy application

--- lib/paginate/railtie.rb
+++ lib/paginate/railtie.rb
@@ -2,7 +2,7 @@ module Paginate
   class Railtie < ::Rails::Railtie
     initializer 'paginate.controller' do
       ActiveSupport.on_load :action_controller_base do
-        ActionController::Base.send :include Paginate::Controller
+        ActionController::Base.send :include, Paginate::Controller
       end
     end
   end
